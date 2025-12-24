2a21de232403038ef238faae9d14279c8b117d86
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-12-24 11:36:22 +0500

    add TypeORM configuration for PostgreSQL database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true, }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'postgres',
+      port: Number(process.env.PG_PORT),
+      username: 'postgres',
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
   ],
   controllers: [],
   providers: [],
